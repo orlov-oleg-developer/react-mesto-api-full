22e74b5336565850e12167d156e2953b2499c14f
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2023-01-05 12:11:23 +0300

    feature: production token check

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # react-mesto-api-full
 Репозиторий для приложения проекта `Mesto`, включающий фронтенд и бэкенд части приложения со следующими возможностями: авторизации и регистрации пользователей, операции с карточками и пользователями. Бэкенд расположите в директории `backend/`, а фронтенд - в `frontend/`. 
+
+[Ссылка на репозиторий](https://github.com/orlov-oleg-developer/react-mesto-api-full)
   
 Пожалуйста, прикрепите в это описание ссылку на сайт, размещенный на Яндекс.Облаке.
+IP  51.250.86.61
+Frontend  https://orlov.developer.nomoredomains.club
+Backend  https://api.orlov.developer.nomoredomains.club
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,34 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const NoAuthError = require('../errors/no-auth-error');
+
+const checkToken = (token) => {
+  const YOUR_JWT = token;
+  const SECRET_KEY_DEV = 'some-secret-key';
+  try {
+    const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+    console.log('\x1b[31m%s\x1b[0m', `
+      Надо исправить. В продакшне используется тот же
+      секретный ключ, что и в режиме разработки.
+    `);
+  } catch (err) {
+    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+      console.log(
+        '\x1b[32m%s\x1b[0m',
+        'Всё в порядке. Секретные ключи отличаются'
+      );
+    } else {
+      console.log(
+        '\x1b[33m%s\x1b[0m',
+        'Что-то не так',
+        err
+      );
+    }
+  }
+}
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,7 +40,8 @@ module.exports = (req, res, next) => {
     }
 
     const token = authorization.replace('Bearer ', '');
-    const payload = jwt.verify(token, JWT_SECRET);
+    checkToken(token);
+    const payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
     req.user = payload;
     next();
   } catch (e) {
